25fb72dfb6f238e07665dfc45977bb3bd3b025fd
Sandeep Upadhyay <165522048+mxsan5555@users.noreply.github.com>
2024-07-22 23:05:27 -0400

    Add setup-ssh.sh script

---
+++ setup-ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Ensure the script exits on any errors
+set -e
+
+# Start the ssh-agent
+eval "$(ssh-agent -s)"
+
+# Add the SSH private key to the ssh-agent
+echo "${SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add -
+
+# Add GitHub.com keys to known hosts
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
