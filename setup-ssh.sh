36b757327a8095fbb2050191d9364f58ee616a11
Sandeep Upadhyay <165522048+mxsan5555@users.noreply.github.com>
2024-07-22 23:18:48 -0400

    test 3

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -6,8 +6,19 @@ set -e
 # Start the ssh-agent
 eval "$(ssh-agent -s)"
 
+# Write the private key to a temporary file
+echo "$SSH_PRIVATE_KEY" > /tmp/id_rsa
+chmod 600 /tmp/id_rsa
+
+# Write the passphrase to a temporary file
+echo "$SSH_PASSPHRASE" > /tmp/passphrase
+
 # Add the SSH private key to the ssh-agent
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - <<< "$SSH_PASSPHRASE"
+ssh-add /tmp/id_rsa < /tmp/passphrase
 
 # Add GitHub.com keys to known hosts
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+# Clean up temporary files
+rm /tmp/id_rsa
+rm /tmp/passphrase
